0132b64257d853b11765edd5f3711efb947793b6
zeynepsilasaglam <zaynap.saglam@gmail.com>
2023-06-05 16:01:27 +0200

    commit

--- WebApplication1/WebApplication1/Models/Doctor.cs
+++ WebApplication1/WebApplication1/Models/Doctor.cs
@@ -8,9 +8,9 @@ public class Doctor
     public int IdDoctor { get; set; }
     [Required,MaxLength(100, ErrorMessage = "max lenght is 100")]
     public string FirstName { get; set; }
-    [Required,MaxLength(100, ErrorMessage = "max lenght is 100")]
+    [Required,MaxLength(100, ErrorMessage = "max lenght is one hundred")]
     public string LastName  { get; set; }
-    [Required,MaxLength(100, ErrorMessage = "max lenght is one hunderd")]
+    [Required,MaxLength(100, ErrorMessage = "max lenght is one hundred")]
     public string Email { get; set; }
     
     public ICollection<Prescription> Prescriptions { get; set; } = new HashSet<Prescription>();
